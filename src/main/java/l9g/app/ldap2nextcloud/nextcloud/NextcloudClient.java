4351e80c11ce1137c7eea1c05ee4f19850c5cbaa
Thorsten Ludewig <t.ludewig@gmail.com>
2025-10-05 19:38:52 +0200

    list all user ids implemented

--- src/main/java/l9g/app/ldap2nextcloud/nextcloud/NextcloudClient.java
+++ src/main/java/l9g/app/ldap2nextcloud/nextcloud/NextcloudClient.java
@@ -16,14 +16,19 @@
 package l9g.app.ldap2nextcloud.nextcloud;
 
 import io.netty.handler.codec.http.HttpResponse;
+import java.util.ArrayList;
 import java.util.List;
 import l9g.app.ldap2nextcloud.model.NextcloudAnonymousUser;
 import l9g.app.ldap2nextcloud.model.NextcloudGroup;
 import l9g.app.ldap2nextcloud.model.NextcloudOrganization;
 import l9g.app.ldap2nextcloud.model.NextcloudRole;
 import l9g.app.ldap2nextcloud.model.NextcloudUser;
+import l9g.app.ldap2nextcloud.model.OcsResult;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.client.RestClient;
 import org.springframework.web.service.annotation.DeleteExchange;
 import org.springframework.web.service.annotation.GetExchange;
 import org.springframework.web.service.annotation.PostExchange;
@@ -33,25 +38,63 @@ import org.springframework.web.service.annotation.PutExchange;
  *
  * @author Thorsten Ludewig (t.ludewig@gmail.com)
  */
-public interface NextcloudClient
+@Slf4j
+@RequiredArgsConstructor
+public class NextcloudClient
 {
-  @GetExchange("/api/v1/groups")
-  public List<NextcloudGroup> groups();
+  private final RestClient restClient;
 
-  @GetExchange("/api/v1/organizations")
-  public List<NextcloudOrganization> organizations();
+  public List<NextcloudGroup> groups()
+  {
+    return null;
+  }
 
-  @GetExchange("/api/v1/roles?page={page}&per_page={perPage}")
+  public List<NextcloudOrganization> organizations()
+  {
+    return null;
+  }
+
   public List<NextcloudRole> roles(
     @PathVariable("page") int page,
     @PathVariable("perPage") int perPage
-  );
+  )
+  {
+    return null;
+  }
 
-  @GetExchange("/api/v1/users?page={page}&per_page={perPage}")
+  public List<String> listAllUserIds()
+  {
+    log.debug("listAllUserIds()");
+
+    OcsResult result = restClient
+      .get()
+      .uri(uriBuilder -> uriBuilder.path("/ocs/v1.php/cloud/users").queryParam("format", "json").build())
+      .retrieve()
+      .body(OcsResult.class);
+
+    // System.out.println("r1: " + result);
+
+    if(result != null
+      && result.getOcs() != null
+      && result.getOcs().getMeta().getStatus() != null
+      && result.getOcs().getMeta().getStatuscode() == 100)
+    {
+      Object users = result.getOcs().getData().get("users");
+      if(users != null && users instanceof List)
+      {
+        return (List)users;
+      }
+    }
+    return new ArrayList<>();
+  }
+
   public List<NextcloudUser> users(
     @PathVariable("page") int page,
     @PathVariable("perPage") int perPage
-  );
+  )
+  {
+    return null;
+  }
 
   /*
   Query Parameters
@@ -69,39 +112,62 @@ public interface NextcloudClient
   - Resultset max size
     - limit
    */
-  @GetExchange("/api/v1/users/search?query={query}")
   public List<NextcloudUser> usersSearch(
     @PathVariable("query") String query
-  );
+  )
+  {
+    return null;
+  }
 
   @GetExchange("/api/v1/users/search?query={property}:{search}")
   public List<NextcloudUser> usersSearch(
     @PathVariable("property") String property,
     @PathVariable("search") String search
-  );
+  )
+  {
+    return null;
+  }
 
   @GetExchange("/api/v1/users/search?query={property}:{search}&limit={limit}")
   public List<NextcloudUser> usersSearch(
     @PathVariable("property") String property,
     @PathVariable("search") String search,
     @PathVariable("limit") int limit
-  );
+  )
+  {
+    return null;
+  }
 
   @DeleteExchange("/api/v1/users/{id}")
-  public HttpResponse usersDelete(@PathVariable(name = "id") int id);
+  public HttpResponse usersDelete(@PathVariable(name = "id") int id)
+  {
+    return null;
+  }
 
   @PutExchange("/api/v1/users/{id}")
   public NextcloudUser usersUpdate(@PathVariable(name = "id") int id,
-    @RequestBody NextcloudUser user);
+    @RequestBody NextcloudUser user)
+  {
+    return null;
+  }
 
   @PutExchange("/api/v1/users/{id}")
   public NextcloudUser usersAnonymize(@PathVariable(name = "id") int id,
-    @RequestBody NextcloudAnonymousUser user);
+    @RequestBody NextcloudAnonymousUser user)
+  {
+    return null;
+  }
 
   @PostExchange("/api/v1/users")
-  public NextcloudUser usersCreate(@RequestBody NextcloudUser user);
+  public NextcloudUser usersCreate(@RequestBody NextcloudUser user)
+  {
+    return null;
+  }
 
   @GetExchange("/api/v1/users/me")
-  public NextcloudUser me();
+  public NextcloudUser me()
+  {
+    return null;
+  }
 
 }
